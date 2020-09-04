dd530f247024df73503ce70b54c8b93130dce0ad
Hannah Angle <hannahcangle@gmail.com>
2020-09-04 20:05:22 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,14 +1,12 @@
 CREATE TABLE projects (
-id INTEGER PRIMARY KEY, 
-title TEXT, 
-category TEXT, 
-funding_goal TEXT, 
+id INTEGER PRIMARY KEY,
+title TEXT,
+category TEXT,
+funding_goal NUMERIC,
 start_date TEXT,
 end_date TEXT
 )
 ;
-
-
 
 CREATE TABLE users (
 id INTEGER PRIMARY KEY
